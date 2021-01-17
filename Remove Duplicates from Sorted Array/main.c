88418445fe702f3f29ea081faf9c3994e331b952
wz <13169685102@163.com>
2021-01-17 12:56:55 +0800

    add sloution

--- Remove Duplicates from Sorted Array/main.c
+++ Remove Duplicates from Sorted Array/main.c
@@ -1,7 +1,32 @@
 #include "stdio.h"
 
+// Given a sorted array, remove the duplicates in place such that each element appear only once
+// and return the new length.
+// Do not allocate extra space for another array, you must do this in place with constant memory.
+// For example, Given input array A = [1,1,2],
+// Your function should return length = 2, and A is now [1,2].
+
+int RemoveDuplicates(int *num, int len)
+{
+    int index = 0;
+    for (int i = 1; i < len; i++) {
+        if (num[index] != num[i])
+            num[++index] = num[i];
+        }
+    return index + 1;
+}
+
+void test_01(void)
+{
+    int num[5] = {1, 1, 2, 2, 3};
+    int len;
+    len = RemoveDuplicates(num, 5);
+    printf("test_01 = %d\n", len);
+}
+
+
 int main()
 {
-    printf("12312341231\r\n");
+    test_01();
     return 0;
 }
